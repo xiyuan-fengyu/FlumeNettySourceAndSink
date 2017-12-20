f986980c5a3ce67f8677a9a75dc0dc456be12e20
xiyuan_fengyu <xiyuan_fengyu@163.com>
2017-12-20 13:21:08 +0800

    增加会话的最大等待时间

--- src/main/java/com/xiyuan/flume/source/NettyClientSource.java
+++ src/main/java/com/xiyuan/flume/source/NettyClientSource.java
@@ -58,7 +58,6 @@ public class NettyClientSource extends AbstractSource implements EventDrivenSour
         List<Event> events = new ArrayList<>();
         for (ByteBuffer buffer : dataPack.getDatas()) {
             events.add(HeaderAndBodySerialization.deserialize(buffer));
-            logger.info(events.get(events.size() - 1).toString());
         }
         appendBatch(events);
     }
